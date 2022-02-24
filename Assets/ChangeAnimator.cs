2260101d34be9a6bc6a0115150a53554efa511d6
banyapon9676 <banyapon9676@gmail.com>
2022-02-25 00:42:40 +0700

    commit

---
+++ Assets/ChangeAnimator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimator : MonoBehaviour
+{
+    
+    public void getAnimatorPatting()
+    {
+        this.GetComponent<Animator>().Play("Patting");
+    }
+
+    public void getAnimatorWalking()
+    {
+        this.GetComponent<Animator>().Play("Walking");
+    }
+
+    public void getAnimatorSlow_Run()
+    {
+        this.GetComponent<Animator>().Play("Slow Run");
+    }
+}
